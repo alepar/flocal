a7382bbcf1a5d1759277048b2c3d360dc6c20aea
atamur <atamur@gmail.com>
2013-05-01 20:39:57 +0200

    use java8

--- src/java/main/ru/alepar/flocal/SympathiesFinderMain.java
+++ src/java/main/ru/alepar/flocal/SympathiesFinderMain.java
@@ -1,27 +1,16 @@
 package ru.alepar.flocal;
 
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import com.google.common.collect.FluentIterable;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListeningExecutorService;
-import com.google.common.util.concurrent.MoreExecutors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.alepar.flocal.http.FlocalClient;
-import ru.alepar.flocal.users.*;
+import ru.alepar.flocal.users.ContainsUserInfoPageParser;
+import ru.alepar.flocal.users.RegexpUserListPageParser;
+import ru.alepar.flocal.users.UserInfoPageParser;
+import ru.alepar.flocal.users.UserListPageParser;
 
 import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
-
-import static com.google.common.base.Predicates.notNull;
+import java.util.stream.Streams;
 
 public class SympathiesFinderMain {
 
@@ -30,34 +19,33 @@ public class SympathiesFinderMain {
     public static void main(String[] args) throws Exception {
         log.info("started");
 
-        final ListeningExecutorService executor = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(200));
+        Streams.intRange(1, 900).boxed().unordered().parallel()
+                .flatMap(pageNo -> {
+                    try {
+                        final FlocalClient flocal = threadLocal.get();
+                        final String page = flocal.getPage("/showmembers.php?Cat=&page=" + pageNo);
+                        return userListPageParser.parse(page).parallelStream();
+                    } finally {
+                        final int curCount = doneCount.incrementAndGet();
+                        System.out.print("l" + curCount + "\r");
+                    }
+                })
+                .map(user -> {
+                    final FlocalClient flocal = threadLocal.get();
+                    try {
+                        final String page = flocal.getPage("/showprofile.php?Cat=&User=" + URLEncoder.encode(user.name, "windows-1251"));
+                        return userInfoPageParser.parse(user, page);
+                    } catch (Exception e) {
+                        log.error("error getting userinfo for " + user.name, e);
+                    } finally {
+                        final int curCount = doneCount.incrementAndGet();
+                        System.out.print("u" + curCount + "\r");
+                    }
+                    return null;
+                })
+                .filter(userInfo -> userInfo != null && userInfo.sympathy)
+                .forEach(userInfo -> log.warn("yay! {} !", userInfo.name));
 
-        final List<Future<List<UserInfo>>> infoFutures = Lists.newArrayList();
-        for (int i=1; i<=900; i++) {
-            infoFutures.add(
-                    Futures.transform(
-                            executor.submit(new UserListPageTask(i)),
-                            new UserInfoFunction()
-                    ));
-
-        }
-
-        final FluentIterable<UserInfo> sympathies = FluentIterable
-                .from(
-                        Iterables.concat(
-                                FluentIterable
-                                        .from(infoFutures)
-                                        .transform(MoreGuava.<List<UserInfo>>unwrapFuture())
-                                        .filter(notNull())
-                        ))
-                .filter(new HasSympathy());
-
-
-        for (UserInfo sympathy : sympathies) {
-            log.warn("yay! {} !", sympathy.name);
-        }
-
-        executor.shutdown();
         log.info("done");
     }
 
@@ -70,53 +58,6 @@ public class SympathiesFinderMain {
         }
     };
 
-    private static class UserListPageTask implements Callable<List<User>> {
-
-        private final int pageNo;
-
-        public UserListPageTask(int pageNo) {
-            this.pageNo = pageNo;
-        }
-
-        @Override
-        public List<User> call() throws Exception {
-            try {
-                final FlocalClient flocal = threadLocal.get();
-                final String page = flocal.getPage("/showmembers.php?Cat=&page=" + pageNo);
-                return userListPageParser.parse(page);
-            } finally {
-                final int curCount = doneCount.incrementAndGet();
-                System.out.print("l" + curCount + "\r");
-            }
-        }
-
-    }
-
     private final static UserInfoPageParser userInfoPageParser = new ContainsUserInfoPageParser();
-    private static class UserInfoFunction implements Function<List<User>, List<UserInfo>> {
-        @Override
-        public List<UserInfo> apply(java.util.List<User> input) {
-                final FlocalClient flocal = threadLocal.get();
-                final ArrayList<UserInfo> userInfos = Lists.newArrayList();
-                for (User user : input) {
-                    try {
-                        final String page = flocal.getPage("/showprofile.php?Cat=&User=" + URLEncoder.encode(user.name, "windows-1251"));
-                        userInfos.add(userInfoPageParser.parse(user, page));
-                    } catch (Exception e) {
-                        log.error("error getting userinfo for "+user.name, e);
-                    } finally {
-                        final int curCount = doneCount.incrementAndGet();
-                        System.out.print("u" + curCount + "\r");
-                    }
-                }
-                return userInfos;
-        }
-    }
 
-    private static class HasSympathy implements Predicate<UserInfo> {
-        @Override
-        public boolean apply(UserInfo input) {
-            return input.sympathy;
-        }
-    }
 }
